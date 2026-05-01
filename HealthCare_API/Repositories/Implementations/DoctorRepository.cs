adcee334bbf34e4a90b4349d3a35f1460e262bb3
evadillo <vadillotirado@gmail.com>
2026-04-30 19:07:53 -0600

    feat: Doctor repo and service implementation

---
+++ HealthCare_API/Repositories/Implementations/DoctorRepository.cs
@@ -0,0 +1,63 @@
+﻿using HealthCare_API.Data;
+using HealthCare_API.Entities;
+using HealthCare_API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthCare_API.Repositories.Implementations
+{
+    public class DoctorRepository : IDoctorRepository
+    {
+        private readonly HealthCareDbContext _dbContext;
+        public DoctorRepository(HealthCareDbContext dbContext) { 
+
+            _dbContext = dbContext;
+        
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var doctor = await _dbContext.Doctors.FirstOrDefaultAsync(d => d.Id == id);
+            if (doctor == null)
+                return false;
+
+            _dbContext.Doctors.Remove(doctor);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<IEnumerable<Doctor>> GetAllAsync()
+        {
+            return await _dbContext.Doctors.ToListAsync();            
+        }
+
+        public async Task<Doctor?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Doctors.FirstOrDefaultAsync(d => d.Id == id);
+             
+        }
+
+        public async Task<Doctor> InsertAsync(Doctor doctor)
+        {
+            await _dbContext.Doctors.AddAsync(doctor);
+            await _dbContext.SaveChangesAsync();
+
+            return doctor;
+        }
+
+        public async Task<Doctor?> UpdateAsync(int id, Doctor doctor)
+        {
+            var existing = await _dbContext.Doctors.FirstOrDefaultAsync(d => d.Id == id);
+            if (existing == null)
+                return null;
+
+            existing.Name = doctor.Name;
+            existing.Specialty = doctor.Specialty;
+            existing.YearsOfExperience = doctor.YearsOfExperience;
+            existing.IsAvailable = doctor.IsAvailable;
+
+            await _dbContext.SaveChangesAsync();
+
+            return existing;
+        }
+    }
+}
---
+++ HealthCare_API/Services/Interfaces/IDoctorService.cs
@@ -0,0 +1,14 @@
+﻿using HealthCare_API.DTOs.Doctor;
+using HealthCare_API.DTOs.Patient;
+
+namespace HealthCare_API.Services.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<DoctorDTO>> GetAllAsync();
+        Task<DoctorDTO?> GetByIdAsync(int id);
+        Task<DoctorDTO> InsertAsync(CreateDoctorDTO dto);
+        Task<DoctorDTO?> UpdateAsync(int id, UpdateDoctorDTO dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
