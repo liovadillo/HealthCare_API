0f8ec7fe413125e1f82770ed8d4f4d1d4e828d8e
evadillo <vadillotirado@gmail.com>
2026-05-08 14:53:15 -0600

    feat: Updated Api Client, post and get patient

--- HealthCare_Client/Program.cs
+++ HealthCare_Client/Program.cs
@@ -4,6 +4,27 @@ using System.Net.Http.Json;
 var client = new HttpClient();
 client.BaseAddress = new Uri("https://healthcare-api-evelio-g9b9h7c6cdhybje5.centralus-01.azurewebsites.net");
 
+var newPatient = new CreatePatientDTO
+{
+    Name = "Mariela",
+    Age = 32,
+    Diagnosis = "SOP",
+    IsActive = true,
+    DateOfBirth = new DateTime(1993, 6, 7)
+};
+
+var response = await client.PostAsJsonAsync("/api/patient", newPatient);
+
+if (response.IsSuccessStatusCode)
+{
+    var created = await response.Content.ReadFromJsonAsync<PatientDTO>();
+    Console.WriteLine($"Created: {created.Id} | {created.Name}");
+}
+else
+{
+    Console.WriteLine($"Error: {response.StatusCode}");
+}
+
 // GET todos los pacientes
 var patients = await client.GetFromJsonAsync<List<PatientDTO>>("/api/patient");
 
